48c5a38846482fc6b10a5ab360ee057f957a7985
Kashane S <ksakhakorn@gmail.com>
2022-05-07 20:08:58 -0700

    worked on the webpack.config. finished the package.json files. getting a better idea of the amount of work I will need to put in

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,21 +9,67 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
+    // entry point for files
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    // output for the bundles created
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // webpack plugin that generates the front end index.html and bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'My Personal Text Editor'
+      }),
+      // this injects the custom built server worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      // this creates a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'My Personal Text Editor',
+        short_name: 'My Personal Text Editor',
+        description: 'Just another text editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
+      // CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // babel-loader allows the use ES6
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
